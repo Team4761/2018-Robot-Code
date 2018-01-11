bc5a263b042183afc73a50705c5a2c66ebe5b7c8
Mathias Kools <mathias@localhost.localdomain>
2018-01-10 20:40:21 -0500

    Add climber subystem with dummy RobotMap values and related methods.
    I added a climber subsystem with some simple methods such as a way
    to check if the motor is stalling or not (assuming there is a similar
    way of climbing until stalling as last year). To accompany this I
    added some RobotMap values as well that are dummy right now.
    
    I made sure to have some methods update SmartDashboard / Shuffleboard
    as well.

---
+++ src/climber/Climber.java
@@ -0,0 +1,71 @@
+package climber;
+
+import org.robockets.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * @author Mathias Kools
+ * This is the climbing subsystem, specifically what allows the robot to lift itself. It could a hook.
+ */
+public class Climber extends Subsystem {
+
+	final double STALLING_THRESHOLD = 70; // Stalling threshold in amperes.
+	final Victor climberMotor;
+	final PowerDistributionPanel pdp;
+	
+	public Climber() {
+		climberMotor = RobotMap.climberMotor;
+		pdp = RobotMap.pdp;
+	}
+	
+	/*
+	 * Detect if the climbing motor is stalling.
+	 * @return	whether the motor is stalling or not. true means it's stalling, false means it's not.
+	 */
+	public boolean isStalling() {
+		return (getClimberMotorCurrent() > STALLING_THRESHOLD);
+	}
+	
+	/*
+	 * Detect the amount of current going to the climber motor.
+	 * @return	the amount of current going from the power distribution panel to the climber motor.
+	 */
+	public double getClimberMotorCurrent() {
+		return pdp.getCurrent(RobotMap.climberMotorPDPChannel);
+	}
+	
+	/*
+	 * Stop / brake the climbing motor(s).
+	 */
+	public void stop() {
+		climberMotor.set(0);
+	}
+	
+	/*
+	 * Set the speed from 0 to 1 of the climber motor.
+	 * @param speed		The exact speed to use with the climber motor.
+	 */
+	public void setMotor(double speed) {
+		climberMotor.set(speed);
+	}
+	
+	// SmartDashboard related commands.
+	
+	/*
+	 * Update different display values on the SmartDashboard / Shuffleboard.
+	 */
+	public void periodicSmartDashboard() {
+		SmartDashboard.putBoolean("Climber stalling", isStalling());
+		SmartDashboard.putNumber("Climber motor current", getClimberMotorCurrent());
+	}
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        // setDefaultCommand(new MySpecialCommand());
+    }
+}
+
--- src/org/robockets/robot/RobotMap.java
+++ src/org/robockets/robot/RobotMap.java
@@ -7,6 +7,9 @@
 
 package org.robockets.robot;
 
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.Victor;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -14,13 +17,7 @@ package org.robockets.robot;
  * floating around.
  */
 public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+	public static PowerDistributionPanel pdp = new PowerDistributionPanel(0); // Zero is the CAN id of the powerdistribution panel.
+	public static Victor climberMotor = new Victor(0);
+	public static int climberMotorPDPChannel = 1; // Dummy value.
 }
