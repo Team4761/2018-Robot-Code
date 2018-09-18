3867e1ff73dae5965e96707d9b6a2923da3ada1a
jakebacker <jbacker42@gmail.com>
2018-09-18 15:52:22 -0400

    Fix travis yet again

--- src/main/java/org/robockets/robot/climber/Climber.java
+++ src/main/java/org/robockets/robot/climber/Climber.java
@@ -60,7 +60,6 @@ public class Climber extends Subsystem {
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
 		// setDefaultCommand(new MySpecialCommand());
-		//setDefaultCommand(new ClimberListener());
 	}
 }
 
